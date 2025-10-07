70808d43e6c57de7e7f02903e97acd3408afab2a
pythonsnake602 <118955679+pythonsnake602@users.noreply.github.com>
2025-10-06 18:52:45 -0700

    Add single file mode

--- README.md
+++ README.md
@@ -21,3 +21,15 @@ quickserve -dir public
 # Serves with HTTPS using the certificate cert.pem and key key.pem
 quickserve -cert cert.pem -key key.pem
 ```
+
+## Single File Mode
+
+Single file mode redirects all requests to a single file, which is useful for quickly transferring a single file from
+one device to another.
+
+To use single file mode, pass the path to a file with the `-file` flag
+
+```shell
+# Uses single file mode to redirect all requests to file.txt
+quickserve -file file.txt
+```
--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"mime"
 	"net/http"
+	"os"
 	"path/filepath"
 	"strings"
 )
@@ -37,19 +39,44 @@ func main() {
 	dirPtr := flag.String("dir", ".", "directory to serve")
 	certFilePtr := flag.String("cert", "", "certificate file to use for TLS")
 	keyFilePtr := flag.String("key", "", "key file to use for TLS")
+	singleFilePtr := flag.String("file", "", "single file mode")
 	flag.Parse()
 
 	isHttps := *certFilePtr != "" && *keyFilePtr != ""
+	isSingleFile := false
+	singleFileUrl := "/" + *singleFilePtr
+
+	if *singleFilePtr != "" {
+		if _, err := os.Stat(*singleFilePtr); errors.Is(err, os.ErrNotExist) {
+			fmt.Printf("File %s does not exist\n", *singleFilePtr)
+			os.Exit(1)
+		} else {
+			fmt.Printf("Using single file mode, redirecting all requests to %s\n", *singleFilePtr)
+			isSingleFile = true
+		}
+	}
+
+	if !isSingleFile {
+		fmt.Printf("Serving %s\n", *dirPtr)
+	}
 
 	if isHttps {
-		fmt.Printf("Serving %s\nListening on: %s:%d (https)\n", *dirPtr, *hostPtr, *portPtr)
+		fmt.Printf("Listening on: %s:%d (https)\n", *hostPtr, *portPtr)
 	} else {
-		fmt.Printf("Serving %s\nListening on: %s:%d\n", *dirPtr, *hostPtr, *portPtr)
+		fmt.Printf("Listening on: %s:%d\n", *hostPtr, *portPtr)
 	}
 
 	fileServer := http.FileServer(http.Dir(*dirPtr))
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
+		if isSingleFile {
+			if r.URL.Path != singleFileUrl {
+				w.Header().Set("Location", singleFileUrl)
+				w.WriteHeader(http.StatusTemporaryRedirect)
+				return
+			}
+		}
+
 		applyHeaders(w, r.URL.Path)
 
 		fileServer.ServeHTTP(w, r)
